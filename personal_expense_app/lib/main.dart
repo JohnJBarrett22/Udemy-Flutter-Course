594c51904dcd909c37e52c4dc38ac0584aa61b8c
John Barrett <johnjbarrett22@gmail.com>
2020-11-04 22:46:50 -0800

    Added image, a new widget in chart.dart, for loop logic to filter through transaction data and overall minor changes to project

--- personal_expense_app/lib/main.dart
+++ personal_expense_app/lib/main.dart
@@ -44,18 +44,18 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _userTransactions = [
-    Transaction(
-      id: '001',
-      title: 'New Shoes',
-      amount: 69.99,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: '002',
-      title: 'Weekly Groceries',
-      amount: 279.99,
-      date: DateTime.now(),
-    ),
+    // Transaction(
+    //   id: '001',
+    //   title: 'New Shoes',
+    //   amount: 69.99,
+    //   date: DateTime.now(),
+    // ),
+    // Transaction(
+    //   id: '002',
+    //   title: 'Weekly Groceries',
+    //   amount: 279.99,
+    //   date: DateTime.now(),
+    // ),
   ];
 
   void _addNewTransaction(String txTitle, double txAmount) {
